4ecb4af1c2e6eeb0b7b017982a4cc852c3a91193
muziyuan <gaomuziyuan@icloud.com>
2024-03-25 02:09:39 -0700

    allow write permission

--- api/server.js
+++ api/server.js
@@ -5,12 +5,10 @@ const cors = require("cors");
 const server = jsonServer.create();
 
 // Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join("db.json");
+const data = fs.readFileSync(filePath, "utf-8");
 
 // Comment out to allow write operations
 const router = jsonServer.router("db.json");
