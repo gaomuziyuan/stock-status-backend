31c8b0689277c9f2c78bd6be818448e8aa9b6e74
muziyuan <gaomuziyuan@icloud.com>
2024-03-25 02:12:54 -0700

    update origin

--- api/server.js
+++ api/server.js
@@ -21,7 +21,7 @@ server.use(middlewares);
 
 server.use(
   cors({
-    origin: "https://paint-kanban.vercel.app/",
+    origin: "https://paint-kanban.vercel.app",
   })
 );
 // Add this before server.use(router)
